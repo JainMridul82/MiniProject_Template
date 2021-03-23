5a0e2dad2b2bf05559c50ece492b80e8342c3ad1
JainMridul82 <47223276+JainMridul82@users.noreply.github.com>
2021-03-23 17:30:23 +0530

    Update complex_calculator.c

--- Example_Programs/programming_concpets/calculator_complex/src/complex_calculator.c
+++ Example_Programs/programming_concpets/calculator_complex/src/complex_calculator.c
@@ -13,3 +13,38 @@ error_t complex_sum(complex_t* cnum1, complex_t* cnum2, complex_t* csum)
     return SUCCESS;
 
 }
+
+error_t complex_diff(complex_t* cnum1, complex_t* cnum2, complex_t* csum)
+{
+    if(NULL == cnum1 || NULL == cnum2)
+        return ERROR_NULL_PTR;
+    
+    
+    csum->real = cnum1->real - cnum2->real;
+    csum->imaginary = cnum1->imaginary - cnum2->imaginary;
+
+    return SUCCESS;
+
+}
+
+error_t complex_multiply(complex_t *cnum1, complex_t *cnum2, complex_t *csum)
+{
+    if (NULL == cnum1 || NULL == cnum2)
+        return ERROR_NULL_PTR;
+
+    csum->real = cnum1->real * cnum2->real - cnum1->imaginary * cnum2->imaginary;
+    csum->imaginary = cnum1->real * cnum2->imaginary + cnum1->imaginary * cnum1->real;
+
+    return SUCCESS;
+}
+
+error_t complex_diff(complex_t *cnum1, complex_t *cnum2, complex_t *csum)
+{
+    if (NULL == cnum1 || NULL == cnum2)
+        return ERROR_NULL_PTR;
+
+    csum->real = cnum1->real / cnum2->real;
+    csum->imaginary = cnum1->imaginary / cnum2->imaginary;
+
+    return SUCCESS;
+}
